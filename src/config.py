ced97acbb3c928ff6ddc109b03b9758edc831bc1
Nickolashaa <gracevnikolaj220@gmail.com>
2026-04-26 23:44:52 +0300

    add QSS_COLORS with theme Catppuccin

--- src/config.py
+++ src/config.py
@@ -1,3 +1,23 @@
 WIDTH = 1280
 HEIGHT = 720
 TITLE = "🥪 EAT LOG 🥪"
+
+QSS_COLORS = {
+    "@bg":            "#1e1e2e",
+    "@bg_secondary":  "#181825",
+    "@bg_elevated":   "#313244",
+
+    "@border":        "#45475a",
+    "@border_focus":  "#cba6f7",
+
+    "@text":          "#cdd6f4",
+    "@text_muted":    "#a6adc8",
+    "@text_disabled": "#6c7086",
+
+    "@primary":       "#cba6f7",
+    "@secondary":     "#89b4fa",
+
+    "@success":       "#a6e3a1",
+    "@warning":       "#f9e2af",
+    "@error":         "#f38ba8",
+}
