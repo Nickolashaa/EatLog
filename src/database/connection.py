c7579818bbcbc45d6c0e4048ec52f9b53c599f4b
Nickolashaa <gracevnikolaj220@gmail.com>
2026-04-26 20:50:21 +0300

    update database connection url

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-engine = create_engine(url="sqlite:///database.db")
+engine = create_engine(url="sqlite:///src/database/database.db")
 session_maker = sessionmaker(bind=engine)
 
 
