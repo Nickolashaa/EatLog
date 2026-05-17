a94607b179db11392227ca459f1d6745ffcc2504
Nickolashaa <gracevnikolaj220@gmail.com>
2026-05-17 05:10:18 +0300

    add new font

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
+from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QApplication
 
 from src.widgets.window import EatLogWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setFont(QFont("Cantarell", 11))
     window = EatLogWindow()
     window.show()
     sys.exit(app.exec())
