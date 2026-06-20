8278d85ef347ec0e9f7b67bf53a449d9bc309a48
Nickolashaa <gracevnikolaj220@gmail.com>
2026-06-20 14:05:41 +0300

    b:api:refactoring sessionmaker

--- server/api/src/di/session.py
+++ server/api/src/di/session.py
@@ -9,7 +9,5 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with session_maker() as session:
         try:
             yield session
-            await session.commit()
-        except Exception:
-            await session.rollback()
-            raise
+        finally:
+            await session.close()
