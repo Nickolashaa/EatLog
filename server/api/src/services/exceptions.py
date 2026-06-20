ab7f8e9ac36d25c64d0a06bd7632464d441b3141
Nickolashaa <gracevnikolaj220@gmail.com>
2026-06-20 14:00:05 +0300

    b:api:refactoring exceptions

--- server/api/src/services/exceptions.py
+++ server/api/src/services/exceptions.py
@@ -1,10 +1,12 @@
-class ObjectNotFound(Exception):
+class BaseException(Exception):
     def __init__(self, message: str, **kwargs: object) -> None:
         super().__init__(message)
         self.details = kwargs
 
 
-class ObjectAlreadyExists(Exception):
-    def __init__(self, message: str, **kwargs: object) -> None:
-        super().__init__(message)
-        self.details = kwargs
+class ObjectNotFound(BaseException):
+    pass
+
+
+class ObjectAlreadyExists(BaseException):
+    pass
