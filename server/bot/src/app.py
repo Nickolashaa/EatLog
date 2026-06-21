ef02b1616bb43b8a6298bfbb0369cdd7512b0236
Nickolashaa <gracevnikolaj220@gmail.com>
2026-06-21 23:22:24 +0300

    server:bot:full work register system

--- server/bot/src/app.py
+++ server/bot/src/app.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher
 
-from .config import BOT_TOKEN
+from .config import API_URL, BOT_TOKEN
+from .graphql_client import Client
 from .routers import start
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+dp["gql_client"] = Client(url=API_URL)
 
 
 dp.include_router(start.router)
--- server/bot/src/routers/start.py
+++ server/bot/src/routers/start.py
@@ -1,13 +1,59 @@
+from uuid import UUID
+
 from aiogram import Router
 from aiogram.filters import CommandObject, CommandStart
 from aiogram.types import Message
 
+from ..graphql_client import (
+    Client,
+    GetUserByTelegramIdUserByTelegramIdUser,
+    UpdateUserInput,
+    UpdateUserUpdateUserUser,
+)
+
 router = Router(name="start")
+
+
+def parse_args(command: CommandObject) -> UUID | None:
+    try:
+        return UUID(command.args)
+    except Exception:
+        return None
 
 
 @router.message(CommandStart())
 async def start(
     message: Message,
     command: CommandObject,
+    gql_client: Client,
 ) -> None:
-    await message.answer(command.args or "Сегодня без аргументов")
+    if message.from_user is None:
+        return
+
+    if command.args is None:
+        get_data = await gql_client.get_user_by_telegram_id(
+            telegram_id=str(message.from_user.id)
+        )
+        user = get_data.user_by_telegram_id
+
+        if isinstance(user, GetUserByTelegramIdUserByTelegramIdUser):
+            await message.answer(f"Привет, {user.name}!")
+            return
+        await message.answer("Привет, незнакомец! Как ты сюда попал?)")
+        return
+
+    id = parse_args(command)
+    if id is None:
+        return
+
+    update_data = await gql_client.update_user(
+        id=id,
+        input=UpdateUserInput(telegramId=str(message.from_user.id)),
+    )
+    updated_user = update_data.update_user
+    if isinstance(updated_user, UpdateUserUpdateUserUser):
+        await message.answer(
+            f"Регистрация успешна! Добро пожаловать, {updated_user.name}"
+        )
+        return
+    await message.answer("Пользователь не найден, увы")
